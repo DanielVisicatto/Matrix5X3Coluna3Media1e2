1343d42e70a87fa99b5d15892ead7ef750670bac
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-08 22:13:34 -0300

    Add project files.

---
+++ Matrix5X3Coluna3Media1e2/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] table = new double [5,3];
+
+Popular();
+Imprimir();
+
+void Popular()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Digite os valores para a posição separados por espaço:");
+        string[] entradas = Console.ReadLine().Split(' ');
+
+        table[i, 0] = double.Parse(entradas[0]);
+        table[i, 1] = double.Parse(entradas[1]);
+        table[i, 2] = (table[i, 0] + table[i, 1]) / 2.0;
+    }
+}
+void Imprimir()
+{
+    for (int linha = 0; linha < 5; linha++)
+    {
+        for (int coluna = 0; coluna < 3; coluna++)
+        {
+            Console.Write("|    {0}     | ", table[linha, coluna].ToString("F1"));
+        }
+        Console.WriteLine();
+    }
+}
